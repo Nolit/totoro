5ccf58a067bf3e8b018814ad9695c2a50a2013fc
Nolit <yaboonjapan@gmail.com>
2017-05-07 16:30:22 +0900

    履歴の取得・保存・削除処理作成

--- app/Http/Controllers/HistoryController.php
+++ app/Http/Controllers/HistoryController.php
@@ -2,10 +2,37 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\History;
+use Illuminate\Http\Request;
+use Mockery\Exception;
+
 class HistoryController extends Controller
 {
     public function index()
     {
-        return 'hello';
+        return History::all(['id', 'source', 'result']);
+    }
+
+    public function store(Request $request)
+    {
+        try{
+            $history = new History;
+            $history->source = $request->get("source");
+            $history->result = $request->get("result");
+            $history->save();
+        }catch(Exception $ex){
+            return ['isSuccess' => false];
+        }
+        return ['isSuccess' => true, 'history' => $history];
+    }
+
+    public function destroy(Request $request, $id)
+    {
+        try{
+            History::destroy($id);
+        }catch(Exception $ex){
+            return ['isSuccess' => false];
+        }
+        return ['isSuccess' => true];
     }
 }
